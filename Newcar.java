8f68d2830e9d939fc0821185ef9aa6d71831cbba
prasannavenkadesan <107316738+prasannavenkadesan@users.noreply.github.com>
2022-06-14 17:37:44 +0530

    Add files via upload

---
+++ Newcar.java
@@ -0,0 +1,20 @@
+package test1package;
+
+public class Newcar {
+	int applyBreak=10;
+	int applyGear=20;
+	boolean switchOnAc=true;
+	int applyAcclerate=30;
+
+	public static void main(String[] args) {
+	Newcar c=new Newcar();
+	System.out.println("10");
+	System.out.println("20");
+	System.out.println("true");
+	System.out.println("30");
+}	
+}
+
+
+
+
