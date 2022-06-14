f43872465411bfe737659c19048ea6800c8e4a97
prasannavenkadesan <107316738+prasannavenkadesan@users.noreply.github.com>
2022-06-14 18:16:20 +0530

    Add files via upload

---
+++ Twowheeler.java
@@ -0,0 +1,31 @@
+package test1package;
+
+public class Twowheeler {
+	int noOfWheels=2;
+	short noOfMirrors=2;
+	long chassisNumber=12345678901L;
+	boolean isPunctured=true;
+	String bikeName="Pulsar";
+	double runningKM=123.45678;
+	
+	public static void main(String[] args) {
+		Twowheeler obj1=new Twowheeler();
+	    System.out.println("No of wheels: " + obj1.noOfWheels);
+	    System.out.println("No of Mirrors:" + obj1.noOfMirrors);
+	    System.out.println("ChasisNumber of Two wheeler:" + obj1.chassisNumber);
+	    System.out.println("isPunctured:" + obj1.isPunctured);
+	    System.out.println("Bikename:" + obj1.bikeName);
+	    System.out.println("runningKM:" + obj1.runningKM);
+	    	
+	    }
+	     
+	     
+		}
+		
+
+		
+	
+
+
+
+
